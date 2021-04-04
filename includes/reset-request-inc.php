9aa65085fcae4e72b3e5e6481eb228d69c7145e7
sabarishreddy99 <64861603+sabarishreddy99@users.noreply.github.com>
2021-04-04 17:13:53 +0530

    Update reset-request-inc.php

--- includes/reset-request-inc.php
+++ includes/reset-request-inc.php
@@ -12,7 +12,7 @@ if(isset($_POST['reset-request-submit'])){
 
   $token = random_bytes(32);
 
-  $url = "http://localhost/gv-official/includes/create-new-password.php?selector=". $selector ."&validator=". bin2hex($token);
+  $url = "http://gv-official-test.herokuapp.com/includes/create-new-password.php?selector=". $selector ."&validator=". bin2hex($token);
 
   $expires = date("U") + 1800;
 
